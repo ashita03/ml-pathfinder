1c1a30174890356658d6dee3306cc05c1bdff78d
Ashita Shetty <66629830+ashita03@users.noreply.github.com>
2020-10-29 00:02:17 +0530

    Add files via upload

---
+++ yolo1.py
@@ -0,0 +1,20 @@
+from yolo import YOLO, detect_video
+from PIL import Image
+import cv2
+import numpy as np
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture(0)
+    yolo=YOLO()
+    while 1:
+        ret, img = cap.read()
+        # img = cv2.imread("./0.jpg")
+        cv2img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  
+        pilimg = Image.fromarray(cv2img)
+        r_image = yolo.detect_image(pilimg)
+        # r_image.show()
+        cv2charimg = cv2.cvtColor(np.array(r_image), cv2.COLOR_RGB2BGR)
+        cv2.imshow('img', cv2charimg)
+        k = cv2.waitKey(30) & 0xff
+    cv2.destroyAllWindows()
+    yolo.close_session()
